e05234cfddce2947aaeed0c5a951ec6b0656a1da
Luca Casadei <casadeiluca30@gmail.com>
2024-01-03 22:47:02 +0100

    Added software quality checker to gradle dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
 	application
 	id("com.github.johnrengelman.shadow") version "8.1.1"
+	id("org.danilopianini.gradle-java-qa") version "1.32.0"
 }
 
 application {
@@ -13,6 +14,20 @@ repositories {
 }
 
 dependencies {
+	compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.3")
+
+    // Maven dependencies are composed by a group name, a name and a version, separated by colons
+    implementation("com.omertron:API-OMDB:1.5")
+    implementation("org.jooq:jool:0.9.15")
+
+    /*
+     * Simple Logging Facade for Java (SLF4J) with Apache Log4j
+     * See: http://www.slf4j.org/
+     */
+    val slf4jVersion = "2.0.10"
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    // Logback backend for SLF4J
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.14")
     val junitVersion = "5.10.1"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
@@ -23,6 +38,6 @@ tasks.test {
     useJUnitPlatform()
     testLogging {
 		events(*(org.gradle.api.tasks.testing.logging.TestLogEvent.values()))
+		showStandardStreams = true
 	}
-    testLogging.showStandardStreams = true
 }
