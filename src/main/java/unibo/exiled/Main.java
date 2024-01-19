e9781bc1348d87cd574d6554bb95eb00234ca31c
Luca Casadei <casadeiluca30@gmail.com>
2024-01-19 21:37:09 +0100

    Renamed the MainView to Main.

---
+++ src/main/java/unibo/exiled/Main.java
@@ -0,0 +1,8 @@
+package unibo.exiled;
+import unibo.exiled.view.GameView;
+
+public class Main {
+    public static void main(String[] args) {
+        new GameView();
+    }
+}
--- src/test/java/unibo/exiled/view/TestView.java
+++ src/test/java/unibo/exiled/view/TestView.java
@@ -1,10 +1,12 @@
 package unibo.exiled.view;
+
 import org.junit.jupiter.api.Test;
+import unibo.exiled.Main;
 
 public class TestView {
     @Test
     void showView(){
-        MainView.main(null);
+        Main.main(null);
     }
 
 }
