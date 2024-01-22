a824864cd444c4bdfb026430bc4a8d052d9ad5f1
MarcoMagnani03 <marco.magnani30@studio.unibo.it>
2024-01-22 14:08:58 +0100

    Added javadoc to MagicMove interface and ElementalType

--- src/main/java/unibo/exiled/model/move/MagicMove.java
+++ src/main/java/unibo/exiled/model/move/MagicMove.java
@@ -2,8 +2,29 @@ package unibo.exiled.model.move;
 
 import unibo.exiled.model.utilities.ElementalType;
 
+/**
+ * The MagicMove interface represents a magical move in the game.
+ */
 public interface MagicMove {
+
+    /**
+     * Gets the name of the magic move.
+     *
+     * @return The name of the magic move.
+     */
     String getName();
+
+    /**
+     * Gets the power of the magic move.
+     *
+     * @return The power of the magic move.
+     */
     double getPower();
+
+    /**
+     * Gets the elemental type of the magic move.
+     *
+     * @return The elemental type of the magic move.
+     */
     ElementalType getType();
 }
