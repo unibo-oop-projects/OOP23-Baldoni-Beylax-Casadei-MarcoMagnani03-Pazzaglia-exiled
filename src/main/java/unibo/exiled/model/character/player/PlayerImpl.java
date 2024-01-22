9d65b56eb6313eb2be01c46ab095bbcb818da333
MarcoMagnani03 <marco.magnani30@studio.unibo.it>
2024-01-22 17:12:05 +0100

    The inventoryView now shows if the inventory is empty

--- src/main/java/unibo/exiled/model/character/player/PlayerImpl.java
+++ src/main/java/unibo/exiled/model/character/player/PlayerImpl.java
@@ -1,7 +1,6 @@
 package unibo.exiled.model.character.player;
 
 import unibo.exiled.model.character.attributes.AttributeFactoryImpl;
-import unibo.exiled.model.character.attributes.AttributeType;
 import unibo.exiled.model.character.attributes.Attributes;
 import unibo.exiled.model.item.Inventory;
 import unibo.exiled.model.item.InventoryImpl;
@@ -27,13 +26,6 @@ public class PlayerImpl implements Player {
 
     public PlayerImpl(){
         this.inventory = new InventoryImpl();
-        ItemFactoryImpl factoryItem= new ItemFactoryImpl();
-        inventory.addItem(factoryItem.createHealingItem("Healing Potion", "A potion that gives healing to the player", 20));
-        inventory.addItem(factoryItem.createHealingItem("Healing Potion", "A potion that gives healing to the player", 20));
-        inventory.addItem(factoryItem.createPowerUpItem("Power up attack potion", "A potion that gives a power up to the player", 20,2,AttributeType.ATTACK));
-        inventory.addItem(factoryItem.createPowerUpItem("Power up defense potion", "A potion that gives a power up to the player", 20,2,AttributeType.DEFENSE));
-        inventory.addItem(factoryItem.createUnUsableItem("Redemption crystal", "A crystal"));
-        inventory.addItem(factoryItem.createUnUsableItem("Redemption crystal", "A crystal"));
         this.moveSet = new MoveSetImpl();
     }
 
