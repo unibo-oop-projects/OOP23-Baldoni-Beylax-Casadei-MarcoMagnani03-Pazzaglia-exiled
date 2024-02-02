acbca7089bf5c189b1c6f7b73f0f987509ed3134
Francesco Pazzaglia <francesco.pazzaglia3@studio.unibo.it>
2024-02-02 20:36:08 +0100

    Fixed checkstyle errors in ItemBase and PowerUpItem classes.

--- src/main/java/unibo/exiled/model/item/PowerUpItem.java
+++ src/main/java/unibo/exiled/model/item/PowerUpItem.java
@@ -5,41 +5,70 @@ import unibo.exiled.model.character.player.Player;
 import unibo.exiled.model.utilities.ItemType;
 
 /**
- * This class represents a usable power up item, 
- * the use class allows the character to power up a certain statistic(for example the attack), 
- * the amount of power up is determined by the powerUpValue.
- * The power ups are not permanent, so they have a duration. 
+ * This class represents a usable power-up item.
+ * The use class allows the character to power up a certain statistic (for
+ * example, attack).
+ * The amount of power-up is determined by the powerUpValue.
+ * Power-ups are not permanent, so they have a duration.
  */
-public class PowerUpItem extends ItemBase implements ItemWithDuration{
+public class PowerUpItem extends ItemBase implements ItemWithDuration {
 
     private final AttributeIdentifier boostedAttribute;
     private final int duration;
     private final double powerUpValue;
 
-    public PowerUpItem(final String name,final String description,final double powerUpValue,final int duration,final AttributeIdentifier boostedAttribute) {
-        super(name, description,ItemType.POWERUP);
-        this.duration=duration;
+    /**
+     * Constructs a PowerUpItem with the specified attributes.
+     *
+     * @param name             The name of the power-up item.
+     * @param description      The description of the power-up item.
+     * @param powerUpValue     The value by which the attribute is powered up.
+     * @param duration         The duration of the power-up effect.
+     * @param boostedAttribute The attribute that is boosted by the power-up.
+     */
+    public PowerUpItem(final String name, final String description, final double powerUpValue, final int duration,
+            final AttributeIdentifier boostedAttribute) {
+        super(name, description, ItemType.POWERUP);
+        this.duration = duration;
         this.boostedAttribute = boostedAttribute;
-        this.powerUpValue=powerUpValue;
+        this.powerUpValue = powerUpValue;
     }
 
     @Override
-    public void use(Player player) {
-        //TODO Ora la gestione va cambiata, bisogna specificare di che attributo si tratta.
-        //player.getAttributes().get(boostedAttribute).setModifier(player.getAttributes().get(boostedAttribute).getModifier().get()+powerUpValue);
+    public void use(final Player player) {
+        // TODO: Update the attribute modification logic based on the specific
+        // attribute.
+        // player.getAttributes().get(boostedAttribute).setModifier(player.getAttributes().get(boostedAttribute).getModifier()
+        // + powerUpValue);
     }
 
+    /**
+     * Gets the duration for which the item's effect lasts.
+     *
+     * @return The duration of the item's effect.
+     */
     @Override
     public int getDuration() {
         return this.duration;
     }
 
+    /**
+     * Get the amount or effectiveness of the item.
+     *
+     * @return Depends on the specific type of item. For example, for a healing
+     *         item, it represents the amount of health restored.
+     */
     @Override
     public double getAmount() {
         return this.powerUpValue;
     }
 
-    public AttributeIdentifier getBoostedAttribute(){
+    /**
+     * Gets the attribute that is boosted by the power-up.
+     *
+     * @return The boosted attribute.
+     */
+    public AttributeIdentifier getBoostedAttribute() {
         return this.boostedAttribute;
     }
 }
