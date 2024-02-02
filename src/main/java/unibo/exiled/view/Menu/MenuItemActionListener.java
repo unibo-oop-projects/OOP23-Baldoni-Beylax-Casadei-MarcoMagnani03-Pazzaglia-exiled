bf53d17a551492c67530a93869cc37e62b25a76b
Luca Casadei <casadeiluca30@gmail.com>
2024-02-02 20:23:53 +0100

    CS: MenuItemActionListener.java

--- src/main/java/unibo/exiled/view/Menu/MenuItemActionListener.java
+++ src/main/java/unibo/exiled/view/Menu/MenuItemActionListener.java
@@ -4,23 +4,37 @@ import unibo.exiled.model.menu.Command;
 import unibo.exiled.view.GameView;
 import unibo.exiled.view.NewGameView;
 
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MenuItemActionListener implements ActionListener {
-    GameView game;
-    NewGameView newGameView;
+/**
+ * The action listener class for the menu. To retrieve input.
+ */
+public final class MenuItemActionListener implements ActionListener {
+    private final GameView game;
+    private final NewGameView newGameView;
 
-    public MenuItemActionListener(GameView game, NewGameView newGameView) {
+    /**
+     * The constructor of the in-game menu action listener.
+     *
+     * @param game        The main game view.
+     * @param newGameView The new-game view.
+     */
+    public MenuItemActionListener(final GameView game, final NewGameView newGameView) {
         super();
         this.game = game;
         this.newGameView = newGameView;
     }
 
-    public void actionPerformed(ActionEvent e) {
+    /**
+     * Processes the input event.
+     *
+     * @param e the event to be processed
+     */
+    public void actionPerformed(final ActionEvent e) {
         if (e.getActionCommand().equals(Command.NEW_GAME.getCommandString())) {
             new GameView().display();
             this.newGameView.hide();
-            // new PlayerClassView().display();
         } else if (e.getActionCommand().equals(Command.CLOSE_MENU.getCommandString())) {
             this.game.hideMenu();
         } else if (e.getActionCommand().equals(Command.QUIT.getCommandString())) {
