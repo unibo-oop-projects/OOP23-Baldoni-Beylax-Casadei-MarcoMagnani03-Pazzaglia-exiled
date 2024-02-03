4b2fb8c1b7dc3c8de75c435235b2510bdc3c9d89
Francesco Pazzaglia <francesco.pazzaglia3@studio.unibo.it>
2024-02-03 11:46:24 +0100

    Fixed checkstyle errors in GameMap, GameMapImpl, Direction and ElementalType classes.

--- src/main/java/unibo/exiled/model/map/GameMapImpl.java
+++ src/main/java/unibo/exiled/model/map/GameMapImpl.java
@@ -1,14 +1,31 @@
 package unibo.exiled.model.map;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 import unibo.exiled.model.utilities.Position;
 
-import java.util.*;
-
+/**
+ * Implementation of the GameMap interface representing a game map with cells of
+ * various types.
+ * The map has specific dimensions, height, and width.
+ */
 public class GameMapImpl implements GameMap {
     private final int height;
     private final int width;
     private final Map<Position, CellType> cellStates;
 
+    /**
+     * Constructs a GameMapImpl with a specified size.
+     *
+     * @param size The size of the map, should be an even number.
+     * @throws IllegalArgumentException if the size of the map is not an even
+     *                                  number.
+     */
     public GameMapImpl(final int size) {
         if (size % 2 == 0) {
             this.height = size;
@@ -21,6 +38,15 @@ public class GameMapImpl implements GameMap {
         }
     }
 
+    /**
+     * Fills the specified cell range with the given cell type.
+     *
+     * @param type  The type of cell to fill the range with.
+     * @param fromY The starting Y coordinate of the range.
+     * @param toY   The ending Y coordinate of the range.
+     * @param fromX The starting X coordinate of the range.
+     * @param toX   The ending X coordinate of the range.
+     */
     private void fillCellRange(final CellType type, final int fromY, final int toY, final int fromX, final int toX) {
         for (int i = fromY; i < toY; i++) {
             for (int j = fromX; j < toX; j++) {
@@ -34,20 +60,34 @@ public class GameMapImpl implements GameMap {
         }
     }
 
+    /**
+     * Fills the cells of the map with different cell types based on a starting area
+     * size.
+     *
+     * @param startingArea The size of the starting area where CellType.PLAINS is
+     *                     filled.
+     */
     private void fillCells(final int startingArea) {
         // Random generation of the game map
         final List<CellType> cellTypes = new ArrayList<>(List.of(CellType.values()));
-        cellTypes.remove(CellType.PLAINS); // Removed CellType.PLAINS from cellTypes because it's the starting player spawn area.
+        cellTypes.remove(CellType.PLAINS); // Removed CellType.PLAINS from cellTypes because it's the starting player
+                                           // spawn area.
         Collections.shuffle(cellTypes);
 
         this.fillCellRange(cellTypes.get(0), 0, this.height / 2, 0, this.width / 2);
         this.fillCellRange(cellTypes.get(1), this.height / 2, this.height, 0, this.width / 2);
         this.fillCellRange(cellTypes.get(2), 0, this.height / 2, this.width / 2, this.width);
         this.fillCellRange(cellTypes.get(3), this.height / 2, this.height, this.width / 2, this.width);
-        this.fillCellRange(CellType.PLAINS, startingArea, this.height - startingArea, startingArea, this.width - startingArea);
+        this.fillCellRange(CellType.PLAINS, startingArea, this.height - startingArea, startingArea,
+                this.width - startingArea);
     }
 
-    //Gets the type of cell.
+    /**
+     * Returns the type of cell in a certain position.
+     * 
+     * @param cell The cell position to get the type of.
+     * @return The CellType representing the type of the selected cell.
+     */
     @Override
     public CellType getCellType(final Position cell) {
         if (this.isInBoundaries(cell)) {
@@ -57,17 +97,35 @@ public class GameMapImpl implements GameMap {
         }
     }
 
+    /**
+     * Checks if the selected position is in the boundaries of the map.
+     * 
+     * @param cell The position to check.
+     * @return True if the position is in the boundaries of the map, false
+     *         otherwise.
+     */
     @Override
     public boolean isInBoundaries(final Position cell) {
         return this.cellStates.containsKey(cell);
     }
 
-    //Dimensions getters
+    // Dimensions getters
+
+    /**
+     * Returns the width of the map.
+     * 
+     * @return An Integer representing the width of the map.
+     */
     @Override
     public int getWidth() {
         return this.width;
     }
 
+    /**
+     * Returns the height of the map.
+     * 
+     * @return An Integer representing the height of the map.
+     */
     @Override
     public int getHeight() {
         return this.height;
