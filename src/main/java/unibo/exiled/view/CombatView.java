f3fb07ce2d8c233f86f3715eb7a6a698c432c917
Luca Casadei <casadeiluca30@gmail.com>
2024-02-03 10:01:16 +0100

    CS: Fixed CombatView.java

--- src/main/java/unibo/exiled/view/CombatView.java
+++ src/main/java/unibo/exiled/view/CombatView.java
@@ -2,23 +2,35 @@ package unibo.exiled.view;
 
 import unibo.exiled.controller.CombatController;
 import unibo.exiled.controller.GameController;
-import unibo.exiled.model.GameModel;
 import unibo.exiled.model.character.enemy.Enemy;
-import unibo.exiled.model.character.enemy.EnemyImpl;
-import unibo.exiled.model.character.player.Player;
 import unibo.exiled.model.move.MagicMove;
 import unibo.exiled.view.items.GameButton;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.util.Optional;
 
-public class CombatView extends JPanel {
+/**
+ * The view that starts when the player engages in a combat with an enemy.
+ */
+public final class CombatView extends JPanel {
     private final CombatController combatController;
     private final JPanel moveSetPanel;
     private final JPanel battlePanel;
+    private final int iconSize = 20;
 
-    public CombatView(final CombatController combatController, GameController gameController, GameView game) {
+    /**
+     * The constructor of the combat view.
+     *
+     * @param combatController The controller that manages the combat.
+     * @param gameController   The controller that manages the whole game.
+     * @param game             The view of the game (Main view).
+     */
+    public CombatView(final CombatController combatController, final GameController gameController, final GameView game) {
         this.combatController = combatController;
 
         this.setLayout(new BorderLayout());
@@ -29,7 +41,7 @@ public class CombatView extends JPanel {
         this.add(this.moveSetPanel, BorderLayout.SOUTH);
         this.add(this.battlePanel, BorderLayout.CENTER);
 
-        for (MagicMove move : this.combatController.getPlayerMoveSet().getMagicMoves()) {
+        for (final MagicMove move : this.combatController.getPlayerMoveSet().getMagicMoves()) {
             JButton moveButton = new GameButton(move.name());
             this.moveSetPanel.add(moveButton);
             moveButton.addActionListener(e -> combatController.attack(true));
@@ -39,15 +51,20 @@ public class CombatView extends JPanel {
         escapeButton.addActionListener(e -> game.hideCombat());
         this.moveSetPanel.add(escapeButton);
 
-        JLabel player = new JLabel();
+        final JLabel player = new JLabel();
         player.imageUpdate(
                 new ImageIcon(gameController.getImagePathOfCharacter(this.combatController.getPlayer()).get(0))
                         .getImage(),
-                ERROR, ALLBITS, ABORT, 20, 20);
+                ERROR, ALLBITS, ABORT, iconSize, iconSize);
         this.battlePanel.add(player);
     }
-    
-    public void setEnemy(Optional<Enemy> enemy) {
+
+    /**
+     * Sets the enemy in the combat controller.
+     *
+     * @param enemy The enemy to set in the controller.
+     */
+    public void setEnemy(final Optional<Enemy> enemy) {
         this.combatController.setEnemy(enemy.get());
     }
 }
