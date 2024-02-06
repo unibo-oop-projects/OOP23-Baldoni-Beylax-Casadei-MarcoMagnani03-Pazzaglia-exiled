6c5e61b5f29e7e4972c24302cbf58a8b741c54e0
MarcoMagnani03 <marco.magnani30@studio.unibo.it>
2024-02-06 14:31:21 +0100

    Added disable method of PowerUpItem

--- src/main/java/unibo/exiled/model/item/ItemWithDuration.java
+++ src/main/java/unibo/exiled/model/item/ItemWithDuration.java
@@ -1,5 +1,7 @@
 package unibo.exiled.model.item;
 
+import unibo.exiled.model.character.player.Player;
+
 /**
  * The ItemWithDuration interface extends the UsableItem interface
  * and defines an additional method to retrieve the duration of an item with a limited effect.
@@ -12,4 +14,15 @@ public interface ItemWithDuration extends UsableItem {
      * @return The duration of the item's effect.
      */
     int getDuration();
+
+    /**
+     * Disable the item when the duration is 0
+     * @param player The player on which the item is disabled. 
+     */
+    void disable(Player player);
+
+    /**
+     * Decrease the duration of the item by 1
+     */
+    void decreaseDuration();
 }
