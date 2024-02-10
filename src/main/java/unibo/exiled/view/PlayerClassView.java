c3ed710f88fe7260f1b385868269c812aa44c897
Luca Casadei <luca.casadei27@studio.unibo.it>
2024-02-10 12:52:38 +0100

    Replaced a resource loaded with a stream, replaced default close operation of the PlayerClassView.

--- src/main/java/unibo/exiled/view/PlayerClassView.java
+++ src/main/java/unibo/exiled/view/PlayerClassView.java
@@ -45,7 +45,7 @@ public final class PlayerClassView {
     public PlayerClassView() {
         this.mainFrame = new JFrame();
         this.mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        this.mainFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.mainFrame.setTitle("The Exiled");
         this.mainFrame.setName("Player class");
         this.mainFrame.setLocationByPlatform(true);
