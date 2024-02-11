3fd555c031599e6fe0ed3940d61a062d972b181f
MarcoMagnani03 <marco.magnani30@studio.unibo.it>
2024-02-11 15:45:51 +0100

    Fixed inventory image dimensions

--- src/main/java/unibo/exiled/view/InventoryView.java
+++ src/main/java/unibo/exiled/view/InventoryView.java
@@ -17,6 +17,7 @@ import javax.swing.JOptionPane;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.io.Serial;
 import java.util.Map;
 import java.util.Optional;
@@ -35,7 +36,7 @@ public final class InventoryView extends JPanel {
     private static final int TOP_BOTTOM_MARGIN = 15;
     private static final int INVENTORY_BUTTON_MARGIN_LEFT_RIGHT = 20;
     private static final int INVENTORY_BUTTON_MARGIN_UP_BOTTOM = 45;
-    private static final int ICON_SIZE = 50;
+    private static final double ICON_SIZE_PERCENTAGE = 0.05;
     private static final int ITEM_BUTTON_FONT_SIZE = 15;
     private final transient GameController gameController;
     private final transient GameView gameView;
@@ -117,6 +118,8 @@ public final class InventoryView extends JPanel {
     private GameButton createItemButton(final String itemName, final int quantity) {
         final GameButton itemButton = new GameButton("");
         itemButton.setFontSize(ITEM_BUTTON_FONT_SIZE);
+        final int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        final int calculatedIconSize = (int) (screenWidth * ICON_SIZE_PERCENTAGE);
         switch (gameController.getItemsController().getItemType(itemName)) {
             case HEALTH:
                 itemButton.setBackground(HEALING_ITEM_COLOR);
@@ -143,8 +146,8 @@ public final class InventoryView extends JPanel {
         }
         final Optional<ImageIcon> itemImage =  ItemNames.getItemImage(itemName);
         if (itemImage.isPresent()) {
-            itemButton.setIcon(new ImageIcon(itemImage.get().getImage().getScaledInstance(ICON_SIZE, ICON_SIZE,
-            java.awt.Image.SCALE_SMOOTH)));
+            itemButton.setIcon(new ImageIcon(itemImage.get().getImage().getScaledInstance(
+                    calculatedIconSize, calculatedIconSize, java.awt.Image.SCALE_SMOOTH)));
         }
         itemButton.addActionListener(e -> handleItemButtonClick(itemName));
         return itemButton;
