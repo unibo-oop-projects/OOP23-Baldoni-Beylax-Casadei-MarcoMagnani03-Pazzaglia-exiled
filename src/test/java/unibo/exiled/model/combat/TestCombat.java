a8ccf22c6ed2589c086046cc56119c7ca3a31704
Manuel Baldoni <78842437+Beylax@users.noreply.github.com>
2024-02-17 19:59:46 +0100

    Added combat tests

--- src/test/java/unibo/exiled/model/combat/TestCombat.java
+++ src/test/java/unibo/exiled/model/combat/TestCombat.java
@@ -1,17 +1,56 @@
 package unibo.exiled.model.combat;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import unibo.exiled.controller.GameController;
+import unibo.exiled.controller.GameControllerImpl;
+import unibo.exiled.model.game.GameModel;
+import unibo.exiled.model.game.GameModelImpl;
+import unibo.exiled.utilities.FontManager;
+import unibo.exiled.utilities.Position;
+import unibo.exiled.view.CombatView;
+import unibo.exiled.view.GameView;
 
 class TestCombat {
+    private static GameModel gameModel;
+    private static CombatModel combatModel;
+    private static GameController gameController;
+    private static final int COLPACCIO_POWER = 6;
+
     @BeforeAll
     static void initialize() {
-
+        gameModel = new GameModelImpl();
+        combatModel = new CombatModelImpl(gameModel);
+        gameController = new GameControllerImpl(gameModel);
+        gameModel.getCharacterModel().getPlayer().get().move(new Position(0, 0));
+        combatModel.newCombat();
+        FontManager.loadFont();
     }
 
     @Test
     void testInitCombat() {
+        assertEquals(new Position(0, 0), combatModel.getCombatPosition());
+        assertEquals(CombatStatus.IDLE, combatModel.getCombatStatus());
+        assertNotNull(combatModel.getEnemy());
+        assertNotNull(combatModel.getPlayer());
+        assertFalse(combatModel.needsPlayerToChangeMove());
+    }
 
+    @Test
+    void testAttack() {
+        final GameView gameView = new GameView(gameController);
+        final CombatView combatView = new CombatView(gameController, gameView);
+        gameView.initializeCombat();
+        final double enemyHealth = gameController.getCombatController().getEnemyHealth();
+        gameController.getCombatController().attack(true, Optional.of("COLPACCIO"), gameController, combatView);
+
+        assertEquals(gameController.getCombatController().getEnemyHealth(), enemyHealth - COLPACCIO_POWER);
     }
 }
